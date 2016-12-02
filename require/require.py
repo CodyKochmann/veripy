3b0f105c1c9396630c4bfc355f4d34dce8ee3ab7
cody <kochmanncody@gmail.com>
2016-12-02 12:29:33 -0500

    added formatting tweaks to all python files

--- require/require.py
+++ require/require.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: cody
 # @Date:   2016-11-30 14:28:01
-# @Last Modified 2016-12-01
-# @Last Modified time: 2016-12-01 19:30:30
+# @Last Modified 2016-12-02
+# @Last Modified time: 2016-12-02 12:27:15
 
 # force assertions to be enabled
 try:
@@ -11,94 +11,107 @@ try:
 except AssertionError:
     pass
 
+
 class require:
     @classmethod
-    def __needed(self,type_needed,obj_recieved):
-        return "\n\n\targ needs to be {}, not {}\n".format(type(type_needed),type(obj_recieved))
+    def __needed(self, type_needed, obj_recieved):
+        return "\n\n\targ needs to be {}, not {}\n".format(
+            type(type_needed),
+            type(obj_recieved)
+        )
 
     #============================================================
     # assertions for types of objects
     #============================================================
 
     @classmethod
-    def function(self,*args):
+    def function(self, *args):
         """ assertion that mandates a function """
         self.tuple(args)
         self.not_empty(args)
         for i in args:
-            assert callable(i), self.__needed(callable,i)
+            assert callable(i), self.__needed(callable, i)
+
     @classmethod
-    def type(self,*args):
+    def type(self, *args):
         """ assertion that mandates a 'type' object """
         self.tuple(args)
         self.not_empty(args)
         for i in args:
-            assert isinstance(i, type), self.__needed(type(str),i)
+            assert isinstance(i, type), self.__needed(type(str), i)
+
     @classmethod
-    def str(self,*args):
+    def str(self, *args):
         """ assertion that mandates a str """
         self.tuple(args)
         self.not_empty(args)
         for i in args:
-            assert isinstance(i, str), self.__needed(str(),i)
+            assert isinstance(i, str), self.__needed(str(), i)
+
     @classmethod
-    def float(self,*args):
+    def float(self, *args):
         """ assertion that mandates a float """
         self.tuple(args)
         self.not_empty(args)
         for i in args:
-            assert isinstance(i, float), self.__needed(float(),i)
+            assert isinstance(i, float), self.__needed(float(), i)
+
     @classmethod
-    def int(self,*args):
+    def int(self, *args):
         """ assertion that mandates a int """
         self.tuple(args)
         self.not_empty(args)
         for i in args:
-            assert isinstance(i, int), self.__needed(int(),i)
+            assert isinstance(i, int), self.__needed(int(), i)
+
     @classmethod
-    def list(self,*args):
+    def list(self, *args):
         """ assertion that mandates a list """
         self.tuple(args)
         self.not_empty(args)
         for i in args:
-            assert isinstance(i, list), self.__needed(list(),i)
+            assert isinstance(i, list), self.__needed(list(), i)
+
     @classmethod
-    def dict(self,*args):
+    def dict(self, *args):
         """ assertion that mandates a dict """
         self.tuple(args)
         self.not_empty(args)
         for i in args:
-            assert isinstance(i, dict), self.__needed(dict(),i)
+            assert isinstance(i, dict), self.__needed(dict(), i)
+
     @classmethod
-    def tuple(self,*args):
+    def tuple(self, *args):
         """ assertion that mandates a tuple """
-        assert isinstance(args, tuple), self.__needed(tuple(),args)
+        assert isinstance(args, tuple), self.__needed(tuple(), args)
         self.not_empty(args)
         for i in args:
-            assert isinstance(i, tuple), self.__needed(tuple(),i)
+            assert isinstance(i, tuple), self.__needed(tuple(), i)
+
     @classmethod
-    def bytearray(self,*args):
+    def bytearray(self, *args):
         """ assertion that mandates a bytearray """
         self.tuple(args)
         self.not_empty(args)
         for i in args:
-            assert isinstance(i, bytearray), self.__needed(bytearray(),i)
+            assert isinstance(i, bytearray), self.__needed(bytearray(), i)
 
     #============================================================
     # assertions for content in the objects
     #============================================================
 
     @classmethod
-    def not_empty(self,*args):
+    def not_empty(self, *args):
         """ assertion that mandates the length is not 0 """
-        assert isinstance(args, tuple), self.__needed(tuple(),args)
+        assert isinstance(args, tuple), self.__needed(tuple(), args)
         assert len(args), "the given {} can not be empty".format(type(args))
         for i in args:
             # test that it supports len()
             len(i)
             assert len(i), "the given {} can not be empty".format(type(i))
+
     @classmethod
-    def is_empty(self,*args):
+    def is_empty(self, *args):
         """ assertion that mandates an empty object with length of 0 """
         self.tuple(args)
         self.not_empty(args)
@@ -111,25 +124,31 @@ class require:
     # assertions for iterables
     #============================================================
 
+    @staticmethod
+    def __is_iterable(i):
+        """ boolean test to see if the argument is iterable """
+        output = False
+        try:
+            iter(i)
+            output = True
+        except:
+            pass
+        return output
+
     @classmethod
-    def iterable(self,*args):
+    def iterable(self, *args):
         """ assertion that mandates a iterable object """
         self.tuple(args)
         self.not_empty(args)
-        def is_iterable(i):
-            output = False
-            try:
-                iter(i)
-                output = True
-            except:
-                pass
-            return output
         for i in args:
-            assert is_iterable(i), "iterable object required, found: {}".format(type(i))
+            assert self.__is_iterable(i), "needed: iterable found: {}".format(
+                type(i))
 
     @classmethod
-    def contains_only(self,i,mandated_type):
-        """ assertion that mandates every instance in an iterable object is a certain type """
+    def contains_only(self, i, mandated_type):
+        """ assertion that mandates every instance
+            in an iterable object is a specified type
+        """
         self.iterable(i)
         self.not_empty(i)
         self.type(mandated_type)
@@ -141,10 +160,15 @@ class require:
     #============================================================
 
     @classmethod
-    def any_of(self,i,*args):
-        """ assertion that mandates that the object is one of any of the given types """
+    def any_of(self, i, *args):
+        """ assertion that mandates that the object
+            is one of any of the given types
+        """
         self.tuple(args)
         self.not_empty(args)
-        self.contains_only(args,type)
-        assert any(isinstance(i, t) for t in args), "recieved: {} and needed one of these: {}".format(type(i),args)
-
+        self.contains_only(args, type)
+        test = any(isinstance(i, t) for t in args)
+        assert test, "recieved: {} and needed one of these: {}".format(
+            type(i),
+            args
+        )
--- require/test.py
+++ require/test.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # @Author: cody
 # @Date:   2016-12-01 11:32:54
-# @Last Modified 2016-12-01
-# @Last Modified time: 2016-12-01 12:13:18
+# @Last Modified 2016-12-02
+# @Last Modified time: 2016-12-02 12:28:58
 
 """
-These are the examples used on the README's table.
+These are the examples used on the README's table that are
+used to test the basic functionality of require.
 """
 
 from require import require
@@ -16,26 +17,26 @@ if __name__ == "__main__":
     #   Test that singular arguments work correctly
     #--------------------------------------------------------
 
-    require.function(lambda:"hello")
+    require.function(lambda: "hello")
     require.type(type(0))
     require.str("hello world")
     require.float(0.0)
     require.int(3)
     require.list(['hello world'])
-    require.dict({"hello":"world"})
-    require.tuple((1,2,3,4))
+    require.dict({"hello": "world"})
+    require.tuple((1, 2, 3, 4))
     require.bytearray(bytearray("hello world"))
-    require.not_empty([1,2,3,4])
+    require.not_empty([1, 2, 3, 4])
     require.is_empty([])
     require.iterable(range(4))
-    require.contains_only([1,2,3,4], int)
+    require.contains_only([1, 2, 3, 4], int)
     require.any_of(4, int, float)
 
     #--------------------------------------------------------
     #   test that each method can take multiple arguments
     #--------------------------------------------------------
 
-    require.function(lambda:"hello", lambda:"world")
+    require.function(lambda: "hello", lambda: "world")
     require.type(type(0), type("hi"))
     require.str("hello", "world")
     require.float(0.0, 5.4)
