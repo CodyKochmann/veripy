b75ef36d78ce60e58a222f9843bdb07658987aee
cody <kochmanncody@gmail.com>
2016-12-15 11:35:16 -0500

    added update notes

---
+++ repo-update-notes.sh
@@ -0,0 +1,21 @@
+# these are notes on how to update the repo on pypi
+
+echo 'dont actually run this file'
+exit
+
+# update the remote repo
+git add .
+git commit -m "yourmessage"
+git push origin master
+
+# tag the current repo
+git tag 0.1 -m "changes to this version"
+git push --tags origin master
+
+# send the repo to the test server
+python setup.py register -r pypitest
+python setup.py sdist upload -r pypitest
+
+# send the repo to the main server
+python setup.py register -r pypi
+python setup.py sdist upload -r pypi
