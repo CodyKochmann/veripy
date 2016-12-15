c0db8356feb3084481eb2a7c30d24e0293d84ae1
cody <kochmanncody@gmail.com>
2016-12-15 12:53:49 -0500

    updated setup.py with the new version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'veripy',
   packages = ['veripy'], # this must be the same as the name above
-  version = '0.2',
+  version = '2016.12.15',
   description = 'Shorthand asserts for those who love predictable code but dont have time for writing every assert in the world.',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/veripy',
-  download_url = 'https://github.com/CodyKochmann/veripy/tarball/0.2',
-  keywords = ['assert', 'shorthand', 'contract', 'require', 'veripy'], # arbitrary keywords
+  download_url = 'https://github.com/CodyKochmann/veripy/tarball/2016.12.15',
+  keywords = ['assert', 'shorthand', 'contract', 'require', 'veripy'],
   classifiers = [],
 )
